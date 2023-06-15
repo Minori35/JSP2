864601ca8ee47e44a724314a26819bcf9e914f4e
SayraPa <98625428+Minori35@users.noreply.github.com>
2023-06-15 10:57:24 -0600

    Arreglos

--- app.js
+++ app.js
@@ -1,43 +1,80 @@
-var arr =[5,4,3,2,1]
+// *********************************Arreglos*************************
 
-console.log(arr);
+// var arr =[5,4,3,2,1]
 
-console.log(arr[0],arr[4],);
+// console.log(arr);
 
-arr.reverse
+// console.log(arr[0],arr[4],);
 
-console.log(arr);
+// arr.reverse
 
-arr = arr.map(function(elem){
-    elem *= elem;
-    return elem
-});
+// console.log(arr);
 
-console.log(arr);
+// arr = arr.map(function(elem){
+//     elem *= elem;
+//     return elem
+// });
 
-arr = arr.map( Math.sqrt);
-console.log(arr);
+// console.log(arr);
 
-
-arr= arr.join("|");
-
-console.log(arr);
-
-
-arr = arr.split("|")
-console.log(arr);
-
-// arr= arr.push("6");
+// arr = arr.map( Math.sqrt);
 // console.log(arr);
 
 
-console.log(arr.toString());
+// arr= arr.join("|");
 
-var elimine = arr.pop();
-console.log(arr, elimine);
+// console.log(arr);
 
-arr.splice( 1, 1, "10", "20");
+
+// arr = arr.split("|")
+// console.log(arr);
+
+// // arr= arr.push("6");
+// // console.log(arr);
+
+
+// console.log(arr.toString());
+
+// var elimine = arr.pop();
+// console.log(arr, elimine);
+
+// arr.splice( 1, 1, "10", "20");
+// console.log(arr);
+
+// arr = arr.splice(0,2)
+// console.log(arr);
+
+
+// *********************************Arreglos*************************
+ 
+var arr =[
+
+     true, 
+     {
+        nombre: "sayra",
+        apellidos : "Herrera"
+     },
+     function(){
+        console.log("Estoy viviendo en un arreglo");
+     },
+     function(persona){
+        console.log(persona.nombre + " " + persona.apellido);
+     },
+     ["Fernando", "Carlos", "Sayra", "Bruno",
+       
+     [
+         "Juan",
+         "Pedro",
+        "otro"]
+    ]
+]
 console.log(arr);
 
-arr = arr.splice(0,2)
-console.log(arr);
+console.log(arr[0]);
+console.log(arr[1]);
+
+arr[2]();
+arr[3](arr[1]);
+
+console.log(arr[4][3][1  ]);
+
